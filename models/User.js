0a75904ec2b87284bc23f2f6b18c4e5a0a25898d
Ariful Islam Arif <islam2305101580@diu.edu.bd>
2026-07-16 21:51:07 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -113,6 +113,15 @@ const userSchema = new mongoose.Schema(
     },
 
     lastLoginAt: Date
+    resetPasswordToken: {
+  type: String,
+  default: null
+},
+
+resetPasswordExpires: {
+  type: Date,
+  default: null
+},
   },
   {
     timestamps: true
