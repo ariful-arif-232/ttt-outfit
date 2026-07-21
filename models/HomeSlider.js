ac8efaddc1f6ce0b1b7ac3cfc4d5e9515ab262e8
Ariful Islam Arif <islam2305101580@diu.edu.bd>
2026-07-22 02:38:29 +0600

    Update HomeSlider.js

--- models/HomeSlider.js
+++ models/HomeSlider.js
@@ -6,6 +6,11 @@ const homeSliderSchema = new mongoose.Schema({
   subtitle: { type: String, trim: true, default: '', maxlength: 120 },
   title: { type: String, trim: true, required: true, maxlength: 180 },
   description: { type: String, trim: true, default: '', maxlength: 500 },
+  descriptionPosition: {
+  type: String,
+  enum: ['left', 'right'],
+  default: 'left'
+},
   buttonText: { type: String, trim: true, default: 'Shop Now', maxlength: 40 },
   buttonLink: { type: String, trim: true, default: '/shop', maxlength: 500 },
   textPosition: { type: String, enum: ['left-top','left-center','left-bottom','center-top','center-center','center-bottom','right-top','right-center','right-bottom'], default: 'left-center' },
