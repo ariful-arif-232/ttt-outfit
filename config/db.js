da69e80fe59af1fbfa840766ae84e19d4ff87b5f
Ariful Islam Arif <islam2305101580@diu.edu.bd>
2026-08-03 21:25:52 +0600

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ async function connectDB() {
   if (!process.env.MONGODB_URI) throw new Error('MONGODB_URI is missing. Add it to .env or Vercel Environment Variables.');
   await mongoose.connect(process.env.MONGODB_URI, {
     serverSelectionTimeoutMS: 10000,
-    maxPoolSize: 10
+    maxPoolSize: 10,
+    minPoolSize: 1
   });
   return mongoose.connection;
 }
