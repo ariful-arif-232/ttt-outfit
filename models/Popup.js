437005084b39dd326de74d3c1942846bef82527d
Ariful Islam Arif <islam2305101580@diu.edu.bd>
2026-08-05 01:52:57 +0600

    Add files via upload

---
+++ models/Popup.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const popupSchema = new mongoose.Schema({
+  image: {
+    url: { type: String, required: true, trim: true },
+    publicId: { type: String, trim: true, default: '' }
+  },
+  title: { type: String, trim: true, default: '', maxlength: 120 },
+  description: { type: String, trim: true, default: '', maxlength: 300 },
+  buttonText: { type: String, trim: true, default: 'Shop Now', maxlength: 40 },
+  buttonLink: { type: String, trim: true, default: '/shop', maxlength: 500 },
+  active: { type: Boolean, default: true, index: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Popup', popupSchema);
